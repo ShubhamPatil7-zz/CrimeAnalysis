edea8641b82c9da33dab562c0a332dbf17048570
Shubham <sbp007@ymail.com>
2019-10-17 22:41:06 -0400

    q

--- merge_datasets.py
+++ merge_datasets.py
@@ -10,21 +10,12 @@ geolocator = Nominatim(user_agent="Shubham")
 
 pd.set_option('display.max_columns', None)
 
-<<<<<<< HEAD
 mass_raw = pd.read_csv("Massachusetts.csv", parse_dates = ['OCCURRED_ON_DATE'])
 mary_raw = pd.read_csv("Maryland.csv", parse_dates = ['Start_Date_Time'])
 
 # Random 1000 samples
 mass_raw = mass_raw.sample(n=10)
 mary_raw = mary_raw.sample(n=10)
-=======
-mass_raw = pd.read_csv("Massachusetts.csv", parse_dates = True)
-mary_raw = pd.read_csv("Maryland.csv", parse_dates = True)
-
-# Random 1000 samples
-mass_raw = mass_raw.sample(n=10)
-mary_raw = mary_raw.sample(n=100)
->>>>>>> 05b63a415d1d33f81950dd4d68a2fd3544041c26
 
 mass_final = pd.DataFrame()
 mary_final = pd.DataFrame()
@@ -46,28 +37,18 @@ mary_final['OffenseCodeGroup'] = mary_raw['Crime Name2']
 mass_final['StreetName'] = mass_raw['STREET']
 mary_final['StreetName'] = mary_raw['Street Name']
 
-<<<<<<< HEAD
 mass_datetime = mass_raw['OCCURRED_ON_DATE']
 mary_datetime = mary_raw['Start_Date_Time']
 # mass_final['OccurredOnDate'] = mass_datetime.dt.date
 # mary_final['OccurredOnDate'] = mary_datetime.dt.date
 # mass_final['OccurredOnTime'] = mass_datetime.dt.time
 # mary_final['OccurredOnTime'] = mary_datetime.dt.time
-=======
-mass_datetime = pd.to_datetime(mass_raw['OCCURRED_ON_DATE'])
-mary_datetime = pd.to_datetime(mary_raw['Start_Date_Time'])
-mass_final['OccurredOnDate'] = mass_datetime.dt.date
-mary_final['OccurredOnDate'] = mary_datetime.dt.date
-mass_final['OccurredOnTime'] = mass_datetime.dt.time
-mary_final['OccurredOnTime'] = mary_datetime.dt.time
->>>>>>> 05b63a415d1d33f81950dd4d68a2fd3544041c26
 
 
 mass_final['PoliceDistrictName'] = mass_raw['DISTRICT']
 mary_final['PoliceDistrictName'] = mary_raw['Police District Number']
 
 def geocode_reverse(lat, lng):
-<<<<<<< HEAD
     if math.isnan(lat) or math.isnan(lng):
         return None
     else:
@@ -78,19 +59,3 @@ def geocode_reverse(lat, lng):
 
 print(mass_raw['OCCURRED_ON_DATE'])
 print(mary_raw['Start_Date_Time'])
-=======
-	if math.isnan(lat) or math.isnan(lng):
-		return None
-	else:
-		return geolocator.reverse(f"{lat}, {lng}").address
-
-mass_final['Address'] = mass_raw.apply(lambda x: geocode_reverse(x['Lat'], x['Long']), axis=1)
-mary_final['ZipCode'] = mary_raw['Police District Number']
-
-print(Geocoder('AIzaSyC9azed9tLdjpZNjg2_kVePWvMIBq154eA').reverse_geocode(42.28995318, -71.06076101))
-print(geocoder.google([42.28995318, -71.06076101], method='reverse'))
-
-print(geolocator.reverse(52.509669, 13.376294))
-print(mass_final)
-# print(mary_final)
->>>>>>> 05b63a415d1d33f81950dd4d68a2fd3544041c26
